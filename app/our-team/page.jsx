5207de9a7729782b54a56d8581bcd12f66423e2e
Simone S. Desco <simonedescode@gmail.com>
2025-01-26 01:51:20 +0000

    Update team page to display detailed member information
    Enhanced the team page layout with a grid system and styled the heading. Updated the fetch request to include related data using `populate=*`. Each team member now displays a photo, name, and description in a structured layout.

--- app/our-team/page.jsx
+++ app/our-team/page.jsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 
 async function getAllTeamMembers() {
-    const MembersPromise = await fetch("http://localhost:1337/api/team-members")
+    const MembersPromise = await fetch("http://localhost:1337/api/team-members?populate=*")
     const members = await MembersPromise.json()
     return members.data
 }
@@ -11,12 +11,16 @@ export default async function Page() {
 
     return(
         <div>
-            <h1>Our Team</h1>
-            <div>
+            <h1 className="text-4xl mb-6 font-bold text-gray-700">Our Team</h1>
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
                 {members.map(member => {
-                    return <div>
-                        <Link href={`/our-team/${member.slug}`} > {member.name}</Link>
-                    </div>
+                    return <Link key={member.id} href={`/our-team/${member.slug}`}>
+                        <img src={`http://localhost:1337${member.photo.formats.medium.url}`} />
+                        <div>
+                            <p>{member.name}</p>
+                            <p>{member.description}</p>
+                        </div>
+                    </Link>
 
                 })}
 
