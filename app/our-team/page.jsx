95b14cc2887bd2082cae629fffab4879c59fa0a1
Simone S. Desco <simonedescode@gmail.com>
2025-01-26 01:24:27 +0000

    Add dynamic team member pages and fetch team data
    Created a dynamic route for team member pages using [slug] to fetch and display individual team member details. Updated the team overview page to fetch all members and link to their respective pages.

--- app/our-team/page.jsx
+++ app/our-team/page.jsx
@@ -1,7 +1,26 @@
-export default function Page() {
+import Link from "next/link";
+
+async function getAllTeamMembers() {
+    const MembersPromise = await fetch("http://localhost:1337/api/team-members")
+    const members = await MembersPromise.json()
+    return members.data
+}
+
+export default async function Page() {
+    const members = await getAllTeamMembers()
+
     return(
         <div>
-            <h1>placeholder3</h1>
+            <h1>Our Team</h1>
+            <div>
+                {members.map(member => {
+                    return <div>
+                        <Link href={`/our-team/${member.slug}`} > {member.name}</Link>
+                    </div>
+
+                })}
+
+            </div>
         </div>
     )
 }
