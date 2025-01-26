1b90ef59f895c207dca4778f9c7ed30e2392b693
Simone S. Desco <simonedescode@gmail.com>
2025-01-26 02:08:46 +0000

    Enhance "Our Team" member cards with new hover effects
    Refactored the layout and styling of team member cards to improve their visual appeal and interactivity. Added hover effects, including scaling and rotation for images, and updated text styling for better readability. This provides a more dynamic and engaging user experience.

--- app/our-team/page.jsx
+++ app/our-team/page.jsx
@@ -14,14 +14,21 @@ export default async function Page() {
             <h1 className="text-4xl mb-6 font-bold text-gray-700">Our Team</h1>
             <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
                 {members.map(member => {
-                    return <Link key={member.id} href={`/our-team/${member.slug}`}>
-                        <img src={`http://localhost:1337${member.photo.formats.medium.url}`} />
-                        <div>
-                            <p>{member.name}</p>
-                            <p>{member.description}</p>
-                        </div>
-                    </Link>
+                    return(
+                        <Link className="group grid grid-cols-[140px_1fr] bg-white rounded-lg overflow-hidden relative hover:bg-gradient-to-r from-white to-amber-50"
+                              key={member.id}
+                              href={`/our-team/${member.slug}`}>
 
+                            <div className="relative overflow-hidden">
+
+                                <img className="transition duration-300 absolute inset-0 h-full w-full object-cover group-hover:scale-125 group-hover:rotate-12"
+                                      src={`http://localhost:1337${member.photo.formats.medium.url}`}/></div>
+                            <div className="p-4">
+                                <p className="text-xl text-gray-900 folt-bold group-hover:text-gray-700">{member.name}</p>
+                                <p className="text-sm text-gray-600 leading-6">{member.description}</p>
+                            </div>
+                        </Link>
+                    )
                 })}
 
             </div>
